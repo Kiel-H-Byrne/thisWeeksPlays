420e4869c6d52bf26e564b5ead15234dc40e87ac
Kiel Byrne <kbyrne@costar.com>
2021-03-26 18:16:22 -0400

    fix comment card

--- components/CommentCard.tsx
+++ components/CommentCard.tsx
@@ -1,22 +1,27 @@
 import { Grid } from "@chakra-ui/react";
+import { useSession } from "next-auth/client";
 import React from "react";
+import MyAvatar from "./MyAvatar";
 
 interface Props {
   uuid: string;
   message: string;
 }
 export const CommentCard = ({ uuid, message }: Props) => {
+  const [session, loading] = useSession();
+
   return (
-    <div key={`${uuid}-${i}`} className={""}>
+    <div key={uuid} className={""}>
       <Grid container direction="row" alignItems="center">
         <Grid item xs={2} md={1}>
-          <img
+
+          {session ? <MyAvatar /> : <img
             alt="image"
-            src={`https://api.adorable.io/avatars/40/${uuid}.pngCopy`}
+            src={`https://avatars.dicebear.com/api/bottts/${uuid}.svg`}
             className={""}
             height="43px"
             width="43px"
-          />
+          />}
         </Grid>
         <Grid item xs={10} md={11}>
           <span>{message}</span>
